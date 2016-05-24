db9b162397749b1c4328d5e58a2efd3f525209db
jbd16 <janer27@gmail.com>
2016-05-24 12:33:32 -0700

    carousel and product partial

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,13 @@
 class StaticPagesController < ApplicationController
   def index
+  	@products = Product.limit(3)
+
   end
+
+  def landing_page
+  	@featured_product = Product.first
+  	@products = Product.limit(3)
+
+  end
+
 end
