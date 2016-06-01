b3a30a7df7f2c0925a491fe5f2cc6234140c0cca
jbd16 <janer27@gmail.com>
2016-06-01 14:42:01 -0700

    email edits

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,10 +10,11 @@ class StaticPagesController < ApplicationController
 
   end
 
-  def thank_you
-  	@name = params[:name]
-  	@email = params[:email]
-  	@message = params[:message]
-  	UserMailer.contact_form(@email, @name, @message).deliver_now
-  end
+def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  UserMailer.contact_form(@email, @name, @message).deliver_now
+end
+ 
 end
