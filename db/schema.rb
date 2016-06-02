cf804fa8a930083e03c048fe7e7dc66f93811181
jbd16 <janer27@gmail.com>
2016-06-02 10:12:49 -0700

    reconfigured routes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160526190630) do
+ActiveRecord::Schema.define(version: 20160526184215) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
