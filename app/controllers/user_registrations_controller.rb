0fd3867d82aac4affed1b799da3e5ab3295b9c3e
jbd16 <janer27@gmail.com>
2016-06-13 15:42:30 -0700

    comment view edits

---
+++ app/controllers/user_registrations_controller.rb
@@ -0,0 +1,9 @@
+class UserRegistrationsController < Devise::UserRegistrationsController
+		def create
+			super
+			if @user.persisted?
+				UserMailer.welcome(@user).deliver_now
+			end
+		end
+	end
+	
