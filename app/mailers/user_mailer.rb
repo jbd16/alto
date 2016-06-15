75855335e5709a1e9cc29fd9406d1af3d9006e87
jbd16 <janer27@gmail.com>
2016-06-14 19:57:03 -0700

    worked with welcome email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,9 +8,9 @@ class UserMailer < ApplicationMailer
         :subject => "A new contact form message from #{name}")
   end
 
-  def welcome(user)
-  	@appname = "Bike Shop"
-  	mail ( :to => user.email,
-  		   :subject => "Welcome to #{@appname}!")
-  end
+ def welcome(user)
+  @appname = "Bike Shop"
+  mail( :to => user.email,
+        :subject => "Welcome to #{@appname}!")
+end
 end
