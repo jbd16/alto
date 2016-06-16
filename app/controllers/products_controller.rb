ecec687f982fe0de449bb28ee2dc661a64bb1227
jbd16 <janer27@gmail.com>
2016-06-15 23:28:52 -0700

    updated products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -63,7 +63,7 @@ end
   def destroy
     @product.destroy
     respond_to do |format|
-      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+      format.html { redirect_to "/products#index", notice: 'Product was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
